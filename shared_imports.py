4a89b501903579f664ec8d66bd433b3dcb5b15d7
Bray <99925439+brayden031@users.noreply.github.com>
2024-10-04 12:54:13 +0100

    Created shared import file
    Contains imports that will be shared across various aspects of the program so can be called simpler.

---
+++ shared_imports.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import requests
+import subprocess
+import time
+import json 
+from datetime import datetime
+from colorama import Fore, Style, init # Font colours
+from prettytable import PrettyTable # Filter results into presentable tables
+table = PrettyTable()
